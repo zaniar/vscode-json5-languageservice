951da6945e8ba387d3d35ae83cbbcdfe527e65cd
Edwin Zaniar Putra <zaniar@nightspade.com>
2020-07-01 11:39:36 +0700

    Remove unused files

--- lib/esm/services/jsonFolding.d.ts
+++
@@ -1,2 +0,0 @@
-import { TextDocument, FoldingRange, FoldingRangesContext } from '../json5LanguageTypes';
-export declare function getFoldingRanges(document: TextDocument, context?: FoldingRangesContext): FoldingRange[];
--- lib/umd/services/jsonDefinition.d.ts
+++
@@ -1,3 +0,0 @@
-import { DefinitionLink, Position, TextDocument, Thenable } from '../json5LanguageTypes';
-import { JSON5Document } from '../parser/json5Parser';
-export declare function findDefinition(document: TextDocument, position: Position, doc: JSON5Document): Thenable<DefinitionLink[]>;
